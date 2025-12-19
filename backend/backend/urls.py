228e38760e96579ee5c80c0b5c307fd523514af6
엄송현 <greenery3699@gmail.com>
2025-12-19 14:36:02 +0900

    feat: klub_alram 생성

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin', admin.site.urls),
     path('api/v1', include('klub_talk.urls')), 
     path('api/chat', include('klub_chat.urls')), 
-    path("api/recommend/", include("klub_recommend.urls")),
+    path("api/recommend", include("klub_recommend.urls")),
+    path("api/alarm", include("klub_alarm.urls")),
 ]
