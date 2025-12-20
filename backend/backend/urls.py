9a6e291ad4c4365e84acfc0918f23bf84e3a9349
song <greenery3699@gmail.com>
2025-12-20 20:53:10 +0900

    fixed: migrations 크래쉬 수정

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,11 +5,11 @@ urlpatterns = [
     # 관리자
     path('admin', admin.site.urls),
     # 책, 모임, 퀴즈 CRUD
-    path('api/v1', include('klub_talk.urls')), 
+    path('api/v1/', include('klub_talk.urls')), 
     # 웹소켓 실시간 채팅 CRUD
-    path('api/chat', include('klub_chat.urls')), 
+    path('api/chat/', include('klub_chat.urls')), 
     # AI API 기반 추천 기능 
-    path("api/recommend", include("klub_recommend.urls")),
+    path("api/recommend/", include("klub_recommend.urls")),
     # 웹소켓 실시간 알림 기능
-    path("api/alarm", include("klub_alarm.urls")),
+    path("api/alarm/", include("klub_alarm.urls")),
 ]
