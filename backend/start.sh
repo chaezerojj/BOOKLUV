b7224c078c5adfd1201c34fc1ab5253ad2fac798
song <greenery3699@gmail.com>
2025-12-25 00:29:07 +0900

    fixed: start.sh 마이그레이션 재시도-교착상태 수정

--- backend/start.sh
+++ backend/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export PYTHONPATH=/app:$PYTHONPATH
 
-# 정적 파일만 수집 (이건 빠릅니다)
+# 빌드할 때나 실행할 때 정적 파일만 준비합니다.
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-# 서버 실행 (Daphne)
-echo "Starting Daphne..."
+# 서버 실행
+echo "Starting Daphne server..."
 exec daphne -b 0.0.0.0 -p ${PORT:-8080} backend.asgi:application
