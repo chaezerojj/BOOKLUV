28eb1b7ab1b8dcdb1de85fd975548c6969f25f1b
엄송현 <greenery3699@gmail.com>
2025-12-24 17:12:58 +0900

    fixed: railway상 python import 에러 수정

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Running Django migrations..."
-python manage.py migrate || { echo "Django migration failed"; exit 1; }
+python3 manage.py migrate || { echo "Django migration failed"; exit 1; }
 
 echo "Starting Django server..."
-python manage.py runserver 0.0.0.0:8000 || { echo "Django server failed"; exit 1; }
+python3 manage.py runserver 0.0.0.0:8000 || { echo "Django server failed"; exit 1; }
 
 echo "Starting Celery worker..."
 celery -A backend worker -l info || { echo "Celery worker failed"; exit 1; }
