c26061f29f79b76b532756def74c5c97d2e93d4b
엄송현 <greenery3699@gmail.com>
2025-12-24 15:38:15 +0900

    fixed : settings. compose.yml 수정

--- start.sh
+++ start.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-# 시작 메시지
-echo "Starting the backend services..."
-
 # Django 마이그레이션 실행
 echo "Running Django migrations..."
-python3 manage.py migrate
+python manage.py migrate
 
-# Django 서버 실행 (0.0.0.0은 외부에서 접근 가능하게 설정)
+# Django 서버 실행
 echo "Starting Django server..."
-python3 manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000 &  # 백그라운드 실행
 
 # Celery 워커 실행
 echo "Starting Celery worker..."
-celery -A backend worker -l info &
+celery -A backend worker -l info &  # 백그라운드 실행
 
 # Celery Beat 실행
 echo "Starting Celery Beat..."
-celery -A backend beat -l info &
+celery -A backend beat -l info &  # 백그라운드 실행
+
+wait  # 모든 백그라운드 프로세스가 끝날 때까지 기다림
