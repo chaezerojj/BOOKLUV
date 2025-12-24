777a076e2e633548f8f6102e98af9e861c72f527
chaezerojj <yyzz1219@gmail.com>
2025-12-25 05:46:02 +0900

    fix: csrfToeken 에러 수정

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 let csrfToken = null;
 
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(";").shift();
-  return null;
-}
+// function getCookie(name) {
+//   const value = `; ${document.cookie}`;
+//   const parts = value.split(`; ${name}=`);
+//   if (parts.length === 2) return parts.pop().split(";").shift();
+//   return null;
+// }
 
 
 export const http = axios.create({
@@ -15,23 +15,24 @@ export const http = axios.create({
   withCredentials: true,
 });
 
-//  CSRF 토큰을 백엔드에서 받아서 메모리에 저장
-export async function ensureCsrfToken() {
-  if (csrfToken) return csrfToken;
-  const res = await http.get("/api/v1/auth/csrf/");
+// CSRF 토큰을 백엔드에서 받아서 메모리에 저장
+export async function ensureCsrfToken(force = false) {
+  if (csrfToken && !force) return csrfToken;
+
+  const res = await http.get("/api/v1/auth/csrf/"); // 너가 만든 csrf 엔드포인트
   csrfToken = res.data?.csrfToken;
   return csrfToken;
 }
 
-http.interceptors.request.use((config) => {
+http.interceptors.request.use(async (config) => {
   const method = (config.method || "get").toLowerCase();
   const unsafe = ["post", "put", "patch", "delete"].includes(method);
-  
+
   if (unsafe) {
-    const csrftoken = getCookie("csrftoken");
-    if (csrftoken) {
+    const token = await ensureCsrfToken();
+    if (token) {
       config.headers = config.headers || {};
-      config.headers["X-CSRFToken"] = csrftoken;
+      config.headers["X-CSRFToken"] = token;
     }
   }
   return config;
